6f61d18905ce1989bdfdf49f1bb919d017c52033
Axel Brian Dip <axelbriandip.rg@gmail.com>
2024-11-07 03:57:59 -0300

    sign up add

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import './styles/App.css'
 import HomePage from './pages/HomePage'
+import SignUpPage from './pages/SignUpPage'
+
+import {
+  Routes,
+  Route,
+  BrowserRouter
+} from 'react-router-dom';
 
 function App() {
   return (
     <>
-      <HomePage/>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<HomePage/>}/>
+          <Route path='/signUp' element={<SignUpPage/>}/>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import LogoGesliga from '../assets/imgs/1.png'
-import '../styles/homePage.css'
+import '../styles/SignUpPage.css'
 
-const HomePage = () => {
+const SignUpPage = () => {
   return (
     <section className='containerHomePage'>
       <div className="hero">
@@ -11,7 +11,11 @@ const HomePage = () => {
       <div className="container-form">
         <form>
           <div className="mb-3">
-            <h3>Ingresar al sistema</h3>
+            <h3>Crear usuario</h3>
+          </div>
+          <div className="mb-3">
+            <label htmlFor="exampleInputName" className="form-label">Nombre y apellido</label>
+            <input type="text" className="form-control" id="exampleInputName" aria-describedby="nameHelp" value={"Axel Dip"}/>
           </div>
           <div className="mb-3">
             <label htmlFor="exampleInputEmail1" className="form-label">Correo</label>
@@ -21,9 +25,9 @@ const HomePage = () => {
             <label htmlFor="InputPassword" className="form-label">Contraseña</label>
             <input type="text" className="form-control" id="InputPassword" aria-describedby="passwordHelp" value={"axel123"}/>
           </div>
-          <button type="submit" className="btn btn-primary">Entrar</button>
+          <button type="submit" className="btn btn-primary">Crear</button>
           <div className="mb-3">
-            <p>¿No tenés cuenta? <a href="/signUp">Crear cuenta</a></p>
+            <p>¿Ya tenés cuenta? <a href="/">Ingresar</a></p>
           </div>
         </form>
       </div>
@@ -31,4 +35,4 @@ const HomePage = () => {
   );
 }
 
-export default HomePage;
+export default SignUpPage;
